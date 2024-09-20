fb8ad04c2af8326024e057a86c3007d1cbb570ec
Vivekjoshi11 <108278744+Vivekjoshi11@users.noreply.github.com>
2024-09-20 22:36:32 +0530

    operators

--- operators.js
+++ operators.js
@@ -20,9 +20,20 @@ console.log("a-- = ", a--)
 // comparision operators
 
 let comp1 = 6;
-let comp2 = 7;
+let comp2 = "7";
 console.log("comp1 == comp2 is ", comp1 == comp2)
 console.log("comp1 != comp2", comp1 != comp2)
 console.log("comp1 === comp2", comp1 === comp2)
 console.log("comp1 !== comp2", comp1 !== comp2)
-console.log("comp1 ")
+console.log("comp1 > comp2 ", comp1>comp2)
+
+// logical opr
+
+let x = 5;
+let y = 6;
+console.log("x && y is ", x && y)
+console.log(x<y && x==5)
+console.log(x>y || x==5)
+
+console.log(!true)
+console.log(!false)
